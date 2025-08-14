36c07b01728bd5543ac84652be68137c6ea66770
Ana Amaral <anacarolinesousadoamaral2020@gmail.com>
2025-08-14 20:05:33 -0300

    list e arraylist

--- ExercicioPaises/src/Main.java
+++ ExercicioPaises/src/Main.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         Pais br = new Pais("BRA", "Brasil", 200000000, 8200000);
@@ -8,11 +12,52 @@ public class Main {
         arg.setPopulacao(8500000);
         arg.setDimensao(7500000);
 
-        System.out.println(br.getNome()+" tem densidade pop. de "+br.getDensidade()+" hab/km2");
+        // Lista Dinâmica
+        /* da pra definir qual tipo ela suporta*/
+        List <Pais> paisesList = new ArrayList();
+        paisesList.add(br);
+        paisesList.add(arg);
+        paisesList.add(new Pais("PY","Paraguay",150000000,22000000));
+        paisesList.add(new Pais("CHI","Chile",250000000,28000000));
+        paisesList.add(0, new Pais("COL","Colombia",38000000,18000000));
+        // add = o brasil foi remanejado para frente, apos a colombia ser adicionada na posicao dele
+        // set = colombia sobrepoe o brasil
+        // addAll = adiciona a lista inteira
+        // a lista aceita tudo, string, inteiro, isso não é bom, pois na hora de recuperar o objeto, retorna um objeto generico
+        System.out.println("Temos "+paisesList.size()+" paises na lista");
+        System.out.println("São eles:");
 
-        if(br.igual(arg))
+        //paisesList.clear(); // zera a lista
+        //System.out.println(paisesList.contains(br)); // se contem o objeto na lista
+        System.out.println(paisesList.indexOf(br)); // posição do objeto na lista
+        //paisesList.remove(br); // remove o objeto e remaneja a lista
+        paisesList.sort(new Comparator<Pais>() {
+
+            @Override
+            public int compare(Pais o1, Pais o2) {
+                return o1.getPopulacao()-o2.getPopulacao();
+            }
+        }); // ordena a lista
+
+        // for tradicional
+        /*for (int i = 0; i < paisesList.size(); i++) {
+            System.out.println(paisesList.get(i).getNome()+" "+paisesList.get(i).getPopulacao());
+        }*/
+
+        // foreach
+        for(Pais pais : paisesList){
+            System.out.println(pais.getNome()+" "+pais.getPopulacao());
+        }
+        System.out.println();
+        paisesList.forEach(pais -> {
+            System.out.println(pais.getNome());
+        });
+
+        //System.out.println(br.getNome()+" tem densidade pop. de "+br.getDensidade()+" hab/km2");
+
+        /*if(br.igual(arg))
             System.out.println("É o mesmo país.");
         else
-            System.out.println("Países diferentes");
+            System.out.println("Países diferentes");*/
     }
 }
