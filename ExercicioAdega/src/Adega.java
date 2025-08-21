9e187c2e52b80f7b371927dca16b8eaef3ef874b
Ana Amaral <anacarolinesousadoamaral2020@gmail.com>
2025-08-21 20:53:40 -0300

    adega

--- ExercicioAdega/src/Adega.java
+++ ExercicioAdega/src/Adega.java
@@ -1,4 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Adega {
+
+    // Record
+    /*private record Posicao(int linha, int coluna){}
+    private Vinho[][] vinho;
+    public Adega(){
+        vinho = new Vinho[20][10];
+    }*/
+
+    // Inner Class
+    public class Posicao {
+        private int linha, coluna;
+
+        public Posicao(int linha, int coluna) {
+            this.linha = linha;
+            this.coluna = coluna;
+        }
+
+        public int getLinha() {
+            return linha;
+        }
+
+        public int getColuna() {
+            return coluna;
+        }
+    }
+
+
     private Vinho[][] vinhos;
     private int quantidadeGarrafas;
     private double valorTotal;
@@ -9,7 +39,7 @@ public class Adega {
         valorTotal = 0.0;
     }
 
-    // colocar o vinho numa posição específica
+    // a) Incluir uma garrafa na adega, dada a posição;
     public boolean add(Vinho vinho, int fila, int col){
         if(fila >= 0  && fila < vinhos.length && col >= 0 && col < vinhos[fila].length){
 
@@ -24,24 +54,26 @@ public class Adega {
         return false;
     }
 
-    // colocar o vinho numa posição qualquer
-    public boolean addAleatorio(Vinho vinho) {
-        if (isCheia()) {
-            return false;
+    // b) Incluir uma garrafa na adega, em uma posição aleatória;
+    public void addAleatorio(Vinho vinho) {
+        boolean isCheia = true;
+        List <Posicao> posicoesLivres = new ArrayList<>();
+        for (int i = 0; i < vinhos.length; i++) {
+            for (int j = 0; j < vinhos[i].length; j++) {
+                if(vinhos[i][j] == null) {
+                    posicoesLivres.add(new Posicao(i,j));
+                    isCheia = false;
+                }
+            }
         }
-
-        List posicoesLivres = getPosicoesLivres();
-        if (posicoesLivres.isEmpty()) {
-            return false;
+        // o mét. de acesso "get" retorna um valor inteiro, então precisa convertê-lo para int
+        if(!isCheia){
+            Posicao posicaoSorteada = posicoesLivres.get((int)(Math.random() * posicoesLivres.size()));
+            vinhos[posicaoSorteada.getLinha()][posicaoSorteada.getColuna()] = vinho;
         }
-
-        Random random = new Random();
-        int indiceAleatorio = random.nextInt(posicoesLivres.size());
-        int[] posicao = posicoesLivres.get(indiceAleatorio);
-
-        return add(vinho, posicao[0], posicao[1]);
     }
 
+    // c) Obter a quantidade de vinhos (por tipo) que estão na adega;
     public int getQuantidadeTipo(String tipo){
         int cont = 0;
         for(int i = 0; i < vinhos.length; i++){
@@ -55,15 +87,19 @@ public class Adega {
         }
         return  cont;
     }
+
+    // f) Obter o vinho mais antigo da adega (menor data de fabricação);
+    public 
+
     // para remover um vinho da posicao, jogar null no valor da posicao dele
     // guardar as posicoes livres e sortear uma delas
 
-    // quantidade total
+    // d) Obter a quantidade de garrafas cadastradas na adega;
     public int getQuantidadeTotal() {
         return quantidadeGarrafas;
     }
 
-    // valor agregado
+    // e) Obter o valor agregado da adega (somatório dos vinhos);
     public double getValorAgregado() {
         return valorTotal;
     }
--- ExercicioAdega/src/Main.java
+++ ExercicioAdega/src/Main.java
@@ -3,20 +3,25 @@ import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
-        Vinho vinho = new Vinho("branco","natal",1000, LocalDate.of(2024,12,25),15);
-        System.out.println("Quantidade de dias: "+vinho.getDataDias());
+
+        Vinho vinho1 = new Vinho("branco","Natal",1000, LocalDate.of(2024,12,25),90);
+        Vinho vinho2 = new Vinho("tinto", "Casa Silva", 750, LocalDate.of(2015, 3, 15), 90.00);
+        Vinho vinho3 = new Vinho("branco", "Concha y Toro", 750, LocalDate.of(2018, 7, 20), 90.00);
 
         Adega adega = new Adega();
-        adega.add(vinho,10,8);
-
-        Vinho vinho2 = new Vinho("tinto", "Casa Silva", 750, LocalDate.of(2015, 3, 15), 89.90);
-        Vinho vinho3 = new Vinho("branco", "Concha y Toro", 750, LocalDate.of(2018, 7, 20), 45.50);
-
+        adega.add(vinho1,10,8);
         adega.add(vinho2, 5, 3);
+        adega.addAleatorio(vinho3);
 
+
+        System.out.println("Quantidade de dias (Vinho 1): "+vinho1.getDataDias());
+        // System.out.println("Quantidade de dias: "+vinho2.getDataDias());
+        // System.out.println("Quantidade de dias: "+vinho3.getDataDias());
         System.out.println("Quantidade de Vinhos Brancos: " + adega.getQuantidadeTipo("branco"));
         System.out.println("Quantidade de Vinhos Tintos: " + adega.getQuantidadeTipo("tinto"));
         System.out.println("Quantidade Total: " + adega.getQuantidadeTotal());
         System.out.println("Valor Agregado: R$" + adega.getValorAgregado());
+
+        //Adega.Posicao pos = new Adega.Posicao(0,0); isso apenas se a classe Posicao() for static
     }
 }
