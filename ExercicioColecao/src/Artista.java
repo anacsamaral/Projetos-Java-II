dfbd5b9f657ca9cb83ad6ba134f85e45bd841275
Ana Amaral <anacarolinesousadoamaral2020@gmail.com>
2025-09-02 10:48:14 -0300

    class CD

--- ExercicioColecao/src/Artista.java
+++ ExercicioColecao/src/Artista.java
@@ -1,4 +1,34 @@
-public class Artista {
-    private String nome;
+import java.util.ArrayList;
+import java.util.List;
 
+public class Artista {
+    // atributos
+    private String nome;
+    private String tipo;
+    private List<CD> cds; // lista de álbuns do artista
+
+    // construtor
+    public Artista(String nome, String tipo) {
+        this.nome = nome;
+        this.cds = new ArrayList<>(); // inicializando a lista de CD's
+    }
+
+    public void addCD(CD cd) {
+        cds.add(cd);
+    }
+
+    public void listarCDs() {
+        System.out.println("CDs do artista " + nome + ":");
+        for(CD cd : cds)
+            System.out.println(" - " + cd.getTitulo());
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Artista: " + nome + " | Tipo : " + tipo;
+    }
 }
---
+++ ExercicioColecao/src/CD.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CD {
+    private String titulo;
+    private int ano;
+    private Artista artista;
+    private List<Musica> musicas;
+
+    public CD(String titulo, int ano, Artista artista) {
+        this.titulo = titulo;
+        this.ano = ano;
+        this.artista = artista;
+        this.musicas = new ArrayList<>();
+
+        artista.addCD(this); // mantém a lista do artista atualizada
+    }
+
+    // métodos
+    public void addMusica(Musica musica) {
+        musicas.add(musica); // na lista de músicas, adicione uma música
+    }
+
+    public void listarMusica() {
+        System.out.println("Músicas do CD \"" + titulo + "\" do artista " + artista.getNome() + ":");
+        for(Musica m : musicas)
+            System.out.println("  - " + m.exibirInfo());
+    }
+
+    public Musica buscarMusica(String nome) {
+        for(Musica m : musicas) {
+            if (m.getNome().equalsIgnoreCase(nome)) {
+                return m;
+            }
+        }
+        return null;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public Artista getArtista() {
+        return artista;
+    }
+
+    @Override
+    public String toString() {
+        return "CD: " + titulo + " | Ano: " + ano + " | Artistas: " + artista.getNome();
+    }
+}
