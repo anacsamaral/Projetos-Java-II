e63b010e29f2de52bc9123ffe5938dd93abddc03
Ana Amaral <99829816+anacsamaral@users.noreply.github.com>
2025-10-16 18:41:11 -0300

    Update MainConexao.java

--- JDBC/src/main/java/org/example/MainConexao.java
+++ JDBC/src/main/java/org/example/MainConexao.java
@@ -1,31 +1,27 @@
 package org.example;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
 public class MainConexao {
     public static void main(String[] args) {
-        Conexao conexao = new Conexao();
-        if(!conexao.conectar("jdbc:postgresql://localhost:5433/","acervo_db","postgres", "postgres123")){
-            System.out.println("Problemas de Conexão: " + conexao.getMensagemErro());
+        Conexao conexao=new Conexao();
+        if(!conexao.conectar("jdbc:postgresql://localhost:5432/","acervo_db","postgres","postgres123")) {
+            System.out.println("Prolemas na conexao "+conexao.getMensagemErro());
         }
-        else {
-            String sql = "INSERT INTO categoria (ca_nome) VALUES ('drama')";
-            if(conexao.manipular(sql))
-                System.out.println("Erro: " + conexao.getMensagemErro());
-
-            sql = "SELECT * FROM categoria";
-            ResultSet resultSet = conexao.consultar(sql);
+        else{
+            String sql="INSERT INTO categoria (ca_nome) VALUES ('drama')";
+            if(!conexao.manipular(sql))
+                System.out.println("Erro...."+conexao.getMensagemErro());
+            sql="SELECT * FROM categoria";
+            ResultSet resultSet=conexao.consultar(sql);
             try {
-                while(resultSet.next()){
-                    System.out.println("ca_nome");
+                while (resultSet.next()) {
+                    System.out.println(resultSet.getString("ca_nome"));
                 }
-            } catch (Exception e){
-                System.out.println("Erro: " + e);
+            }catch (Exception e){
+                System.out.println("Erro "+e);
             }
-
         }
+
     }
 }
