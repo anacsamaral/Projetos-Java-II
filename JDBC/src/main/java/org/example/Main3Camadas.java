b8697367fa3b3bd2ca52cde01f692a59e29a44f4
Ana Amaral <99829816+anacsamaral@users.noreply.github.com>
2025-10-16 18:49:58 -0300

    Update Main3Camadas.java

--- JDBC/src/main/java/org/example/Main3Camadas.java
+++ JDBC/src/main/java/org/example/Main3Camadas.java
@@ -1,13 +1,25 @@
 package org.example;
-
 import java.sql.ResultSet;
+import java.util.List;
+
 
 public class Main3Camadas {
-    SingletonDB.conectar();
     public static void main(String[] args) {
-        CategoriaDal dal = new CategoriaDal();
-        Categoria categoria = new Categoria("Fantasia");
-        if(!dal.gravar(categoria))
-            System.out.println("Erro ao Gravar");
+        if(!SingletonDB.conectar()){
+            System.out.println(SingletonDB.getConexao().getMensagemErro());
+        }
+        CategoriaDal dal=new CategoriaDal();
+        //Categoria categoria=new Categoria("fantasia");
+
+        //if(!dal.gravar(categoria))
+        //    System.out.println("Erro ao gravar");
+
+        Categoria categoria=dal.get(6);
+        dal.apagar(categoria);
+        //categoria.setNome(categoria.getNome().toUpperCase());
+        //dal.alterar(categoria);
+        List<Categoria> categoriaList = dal.get("");
+        for(Categoria c : categoriaList)
+            System.out.println(c.getNome());
     }
 }
