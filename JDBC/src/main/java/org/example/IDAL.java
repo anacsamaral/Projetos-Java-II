914597c4995c7a435629cfbd2567aba87389bdef
Ana Amaral <99829816+anacsamaral@users.noreply.github.com>
2025-10-16 18:45:45 -0300

    Update IDAL.java

--- JDBC/src/main/java/org/example/IDAL.java
+++ JDBC/src/main/java/org/example/IDAL.java
@@ -8,5 +8,5 @@ public interface IDAL <T>{
     public boolean alterar(T entidade);
     public boolean apagar(T entidade);
     public T get(int id);
-    public List<T> get(String filtro);    
+    public List<T> get(String filtro);
 }
