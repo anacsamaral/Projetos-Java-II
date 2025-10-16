117530f9db2741640fad22ed1cf13b9689107aeb
Ana Amaral <99829816+anacsamaral@users.noreply.github.com>
2025-10-16 18:47:27 -0300

    Update CategoriaDal.java

--- JDBC/src/main/java/org/example/CategoriaDal.java
+++ JDBC/src/main/java/org/example/CategoriaDal.java
@@ -1,50 +1,63 @@
 package org.example;
 
-import javax.xml.transform.Result;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CategoriaDal implements IDAL<Categoria>{
     @Override
     public boolean gravar(Categoria categoria){
-        String sql = "INSERT INTO categoria (ca_nome) VALUES ('#1')"; // token que vai ser substituido no replace
-        sql = sql.replace("#1", categoria.getNome());
-
-        boolean ok = false;
-
-        Conexao conexao = new Conexao();
-        if(!conexao.conectar("jdbc:postgresql://localhost:5433/","acervo_db","postgres", "postgres123"))
-            System.out.println("Problemas de Conexão: " + conexao.getMensagemErro());
-        else
-            ok = conexao.manipular(sql);
-
-        return ok;
-
+        String sql="INSERT INTO categoria (ca_nome) VALUES ('#1')";
+        sql=sql.replace("#1", categoria.getNome());
+        Conexao conexao=new Conexao();
+        boolean ok=false;
+        return SingletonDB.getConexao().manipular(sql);
     }
 
     @Override
     public boolean alterar(Categoria categoria) {
-        String sql = "UPDATE categoria SET ca_nome = '#1' WHERE ca_id = #2";
-        sql = sql.replace("#1", categoria.getNome());
-        sql = sql.replace("#1", "" + categoria.getNome());
-
+        String sql="UPDATE categoria SET ca_nome='#1' WHERE ca_id=#2";
+        sql=sql.replace("#1", categoria.getNome());
+        sql=sql.replace("#2", ""+categoria.getId());
         return SingletonDB.getConexao().manipular(sql);
     }
 
     @Override
     public boolean apagar(Categoria categoria) {
-        return SingletonDB.getConexao().manipular("DELETE FROM categoria WHERE ca_id" + categoria.getId());
+        return SingletonDB.getConexao().manipular("DELETE FROM categoria WHERE ca_id="+categoria.getId());
     }
 
     @Override
     public Categoria get(int id) {
-        Categoria categoria = null;
-        String sql = "SELECT * FROM categoria WHERE ca_id = "+id;
-        ResultSet resultSet =
+        Categoria categoria=null;
+        String sql="SELECT * FROM categoria WHERE ca_id="+id;
+        ResultSet resultSet=SingletonDB.getConexao().consultar(sql);
+        try {
+            if (resultSet.next()) {
+                categoria = new Categoria(resultSet.getInt("ca_id"), resultSet.getString("ca_nome"));
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return categoria;
     }
 
     @Override
     public List<Categoria> get(String filtro) {
-        return List.of();
+        List <Categoria> categoriaList=new ArrayList<>();
+        String sql="SELECT * FROM categoria";
+        if(!filtro.isEmpty())
+            sql+=" WHERE "+filtro;
+        ResultSet resultSet=SingletonDB.getConexao().consultar(sql);
+        try {
+            while (resultSet.next()) {
+                Categoria categoria = new Categoria(resultSet.getInt("ca_id"), resultSet.getString("ca_nome"));
+                categoriaList.add(categoria);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return categoriaList;
     }
 }
