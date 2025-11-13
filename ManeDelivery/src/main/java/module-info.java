dc2edd311fb520900c352b1ae44a29ea48ef1d09
Ana Amaral <99829816+anacsamaral@users.noreply.github.com>
2025-11-13 18:59:46 -0300

    update

--- ManeDelivery/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.manedelivery {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.example.manedelivery to javafx.fxml;
-    exports org.example.manedelivery;
-}
